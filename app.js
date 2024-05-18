a098f2ec17c7ed5c6e1c7ee76e0a1d1fa0f11d09
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-05-18 22:09:39 +0700

    update function

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 const progress = document.getElementById('progress')
-console.log("🚀 ~ progress:", progress)
 const song = document.getElementById('song')
-console.log("🚀 ~ song:", song)
 const ctrlIcon = document.getElementById('ctrlIcon')
 const btnPlayPause = document.getElementById('btnPlayPause')
 
 song.onloadeddata = function () {
-    progress.max = song.duration
-    progress.value = song.currentTime
+    progress.max = song.duration;
+    progress.value = song.currentTime;
 }
 
 btnPlayPause.onclick = function () {
     if (ctrlIcon.classList.contains('fa-play')) {
-        song.play()
-        ctrlIcon.classList.add('fa-pause')
-        ctrlIcon.classList.remove('fa-play')
+        song.play().then(() => {
+            ctrlIcon.classList.add('fa-pause');
+            ctrlIcon.classList.remove('fa-play');
+            setInterval(() => {
+                progress.value = song.currentTime;
+            }, 500);
+        }).catch(error => {
+            console.error('Playback failed:', error);
+        });
     } else {
-        song.pause()
-        ctrlIcon.classList.add('fa-play')
-        ctrlIcon.classList.remove('fa-pause')
+        song.pause();
+        ctrlIcon.classList.add('fa-play');
+        ctrlIcon.classList.remove('fa-pause');
     }
 }
-if (song.play()) {
-    setInterval(() => {
-        progress.value = song.currentTime
-    }, 500)
-}
+
 progress.onchange = function () {
-    song.play()
-    song.currentTime = progress.value
-    ctrlIcon.classList.add('fa-pause')
-    ctrlIcon.classList.remove('fa-play')
+    song.currentTime = progress.value;
+    song.play();
+    ctrlIcon.classList.add('fa-pause');
+    ctrlIcon.classList.remove('fa-play');
 }
